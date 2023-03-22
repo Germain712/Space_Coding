2806ef10ea3f54de42e6607170a55c30f7942c81
Germain712 <102153309+Germain712@users.noreply.github.com>
2023-03-22 21:13:35 +0000

    completed - just need to fix the day symbol

---
+++ script.js
@@ -0,0 +1,11 @@
+const onoff = document.querySelector('.on-off');
+const day = document.querySelector('.uil-brightness');
+const dark = document.querySelector('.uil-moon');
+const bottom = document.querySelector('.bottom');
+
+onoff.onclick = () => {
+ document.body.classList.toggle('change');
+ day.classList.toggle('cday');
+ dark.classList.toggle('cdark');
+ bottom.classList.toggle('dark');
+}
